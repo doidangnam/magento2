ecb56822355b9cc98884e94416836e64c46bd3d7
doinam <doidangnam058@gmail.com>
2022-03-29 17:58:29 +0700

    Chapter 8: Extending the shipping method features

--- app/code/Packt/Shipme/Model/Carrier/Shipme.php
+++ app/code/Packt/Shipme/Model/Carrier/Shipme.php
@@ -75,4 +75,8 @@ class Shipme extends \Magento\Shipping\Model\Carrier\AbstractCarrier implements 
     {
         return ['shipme' => $this->getConfigData('name')];
     }
+
+    public function isTrackingAvailable() {
+        return true;
+    }
 }
